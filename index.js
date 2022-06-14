cee1834a85f5ee35e3c9b3a7c8f1034ed0fd872b
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-14 23:07:39 +0300

    commit tree dbea2e154b8c47b84e639c76e534943be436f9d3

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+// const formData = {
+// 	dogName: "Byron",
+// 	dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+// 	method: "POST",
+// 	headers: {
+// 	  "Content-Type": "application/json",
+// 	  Accept: "application/json",
+// 	},
+// 	body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+// 	.then(function (response) {
+// 	  return response.json();
+// 	})
+// 	.then(function (object) {
+// 	  console.log(object);
+// 	});
+
+const submitData = (username,useremail)=>{
+	return fetch('http://localhost:3000/users',{
+		 method:'POST',
+		 headers:{
+			 'content-Type':'application/json',
+			 Accept:'application/json'
+		 },
+		 body:JSON.stringify({
+			 name:username,
+			 email:useremail
+		 })
+	 })
+	 .then((response)=>response.json())
+	 .then((data)=>{
+		 let body = document.querySelector('body')
+		 body.innerHTML = data.id
+	  console.log(data)
+	 })
+	 .catch(()=>{
+		 document.querySelector('body').innerHTML = 'Unauthorized Access'
+	 })
+  }
